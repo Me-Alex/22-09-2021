a0fb63db5d42680f217a87a39ea4ce11a6dfb4b0
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2021-09-22 06:09:12 +0300

    2nd commit --take that future me -- 10 for naming

--- js/js.js
+++ js/js.js
@@ -19,21 +19,41 @@ menu.onfocus = () => {
     navigation.style.borderRadius = "20px";
     navigation.style.padding = "20px";
     navigation.style.gap = "2px";
-    forMenu.style.zIndex="300";
+    forMenu.style.zIndex = "300";
     // navigation.style.zIndex = "-200";
 
 }
 
 forMenu.onclick = () => {
     navigation.style = " ";
-    forMenu.style.zIndex="-300";
+    forMenu.style.zIndex = "-300";
 
 }
-let yes= document.querySelector(".yes");
-let no= document.querySelector(".no");
-// yes.onclick=()=>{
+let yes = document.querySelector(".Yes");
+let no = document.querySelector(".No");
+let yes1 = document.querySelector(".Yes1");
+let no1 = document.querySelector(".No1");
+let downlaodButton = document.querySelector(".download-button");
+let text = document.querySelector(".text");
+let tips = document.querySelector(".tips");
 
-// }
-// no.onclick=()=>{
-    
-// }
+
+yes.onclick = () => {
+    downlaodButton.style.display = "inherit";
+    text.style.display = "none";
+}
+no.onclick = () => {
+    downlaodButton.style.display = "none";
+    text.style.display = "none";
+    tips.style.display = "flex";
+}
+yes1.onclick = () => {
+    downlaodButton.style.display = "inherit";
+    text.style.display = "none";
+    tips.style.display = "none";
+
+}
+no1.onclick = () => {
+    downlaodButton.style.display = "none";
+    text.style.display = "none";
+}
